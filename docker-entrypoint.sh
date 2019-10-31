17c60a24f3eee48a51c6fd6400439b1dbd7d40b0
Gerald Sim <gerald@meappy.com>
2019-11-01 10:29:05 +1030

    Fixed up BASIC_AUTH var prefix in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Create nginx user/pass
-if [ -z "${BASIC_AUTH_USERNAME}" ] || [ -z "${BASIC_AUTH_PASSWORD}" ]; then
+if [ -z "${AUTH_USERNAME}" ] || [ -z "${AUTH_PASSWORD}" ]; then
   :
 else
-  printf "${BASIC_AUTH_USERNAME:-admin}:$(openssl passwd -apr1 ${BASIC_AUTH_PASSWORD:-MzlmaE1KS2RKKzByV0l2TDVNeS9ML2NU})\n" \
+  printf "${AUTH_USERNAME:-admin}:$(openssl passwd -apr1 ${AUTH_PASSWORD:-MzlmaE1KS2RKKzByV0l2TDVNeS9ML2NU})\n" \
          > /etc/nginx/conf.d/.htpasswd
   perl -p -i -e "s{(^  \})}{    auth_basic "Realm";\n    auth_basic_user_file conf.d/.htpasswd;\n\1}g" /etc/nginx/conf.d/default.conf
 fi
